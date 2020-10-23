b4aca0fc02d95eb0c9b40d4a503a56fede724837
PGrover2001 <40870043+PGrover2001@users.noreply.github.com>
2020-10-23 23:05:37 +0100

    added the video section and styled it

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,11 +1,118 @@
 import React from "react";
 import "./RecommendedVideos.css";
+import VideoCard from "./VideoCard";
+
 function RecommendedVideos() {
   return (
     <div className="recommendedVideos">
-      <h1>hello</h1>
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard
+          title={`Do You Wanna Bang?" Prank"`}
+          image="https://i.ytimg.com/vi/ec2bWfOxsV8/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDueYIz-Sefm6ykgeGDvhwwV3hj4w"
+          channel="TikTokompilation"
+          channelImage="https://yt3.ggpht.com/a/AATXAJyS5qJg3WvouYrcie7_RC90lS_jtwcEghSyX2Ce=s48-c-k-c0xffffffff-no-rj-mo"
+          views="82.2K"
+          timestamp="2 days ago"
+        />
+
+        <VideoCard
+          title={`calling code bootcamps pretending I can't code...(They STRUGGLE pretty hard)"`}
+          image="https://i.ytimg.com/vi/SR1oCu9RhWI/hqdefault.jpg?sqp=-oaymwEZCOADEI4CSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCsauMDdu7PESeY_l4KbyVBFOybMA"
+          channel="Joshua Fluke"
+          channelImage="https://yt3.ggpht.com/a/AATXAJzsLfjQGNpQtVWS4hXGxM6F21cdArOL-sTIu9rOXw=s48-c-k-c0xffffffff-no-rj-mo"
+          views="84.2K"
+          timestamp="5 days ago"
+        />
+
+        <VideoCard
+          title={`🔴 Build a Whatsapp Clone with MERN Stack (MongoDB, Express, React, Node JS)`}
+          image="https://i.ytimg.com/vi/gzdQDxzW2Tw/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAu_J66n0_P1jy1eQlsgocO_zz9pQ"
+          channel="Clever Programmer"
+          channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+          views="140k"
+          timestamp="7 days ago"
+        />
+        <VideoCard
+          title={`TRASH or PASS! Joyner Lucas ( Snitch ) Evolution [REACTION!!!]`}
+          image="https://i.ytimg.com/vi/b8KxKTQSRU8/hq720.jpg?sqp=-oaymwEZCOgCEMoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLALUF8sOItosgOGNO-Yz21RIlnsAA"
+          channel="LayedBakDFR"
+          channelImage="https://yt3.ggpht.com/a/AATXAJxbd79agwiyCofooDIJh8HS0oN60wrEqNBXttsmQQ=s48-c-k-c0xffffffff-no-rj-mo"
+          views="115k"
+          timestamp="1 day ago"
+        />
+        <VideoCard
+          title={`Do You Wanna Bang?" Prank"`}
+          image="https://i.ytimg.com/vi/ec2bWfOxsV8/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDueYIz-Sefm6ykgeGDvhwwV3hj4w"
+          channel="TikTokompilation"
+          channelImage="https://yt3.ggpht.com/a/AATXAJyS5qJg3WvouYrcie7_RC90lS_jtwcEghSyX2Ce=s48-c-k-c0xffffffff-no-rj-mo"
+          views="82.2K"
+          timestamp="2 days ago"
+        />
+
+        <VideoCard
+          title={`calling code bootcamps pretending I can't code...(They STRUGGLE pretty hard)"`}
+          image="https://i.ytimg.com/vi/SR1oCu9RhWI/hqdefault.jpg?sqp=-oaymwEZCOADEI4CSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCsauMDdu7PESeY_l4KbyVBFOybMA"
+          channel="Joshua Fluke"
+          channelImage="https://yt3.ggpht.com/a/AATXAJzsLfjQGNpQtVWS4hXGxM6F21cdArOL-sTIu9rOXw=s48-c-k-c0xffffffff-no-rj-mo"
+          views="84.2K"
+          timestamp="5 days ago"
+        />
+
+        <VideoCard
+          title={`🔴 Build a Whatsapp Clone with MERN Stack (MongoDB, Express, React, Node JS)`}
+          image="https://i.ytimg.com/vi/gzdQDxzW2Tw/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAu_J66n0_P1jy1eQlsgocO_zz9pQ"
+          channel="Clever Programmer"
+          channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+          views="140k"
+          timestamp="7 days ago"
+        />
+        <VideoCard
+          title={`TRASH or PASS! Joyner Lucas ( Snitch ) Evolution [REACTION!!!]`}
+          image="https://i.ytimg.com/vi/b8KxKTQSRU8/hq720.jpg?sqp=-oaymwEZCOgCEMoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLALUF8sOItosgOGNO-Yz21RIlnsAA"
+          channel="LayedBakDFR"
+          channelImage="https://yt3.ggpht.com/a/AATXAJxbd79agwiyCofooDIJh8HS0oN60wrEqNBXttsmQQ=s48-c-k-c0xffffffff-no-rj-mo"
+          views="115k"
+          timestamp="1 day ago"
+        />
+        <VideoCard
+          title={`Do You Wanna Bang?" Prank"`}
+          image="https://i.ytimg.com/vi/ec2bWfOxsV8/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDueYIz-Sefm6ykgeGDvhwwV3hj4w"
+          channel="TikTokompilation"
+          channelImage="https://yt3.ggpht.com/a/AATXAJyS5qJg3WvouYrcie7_RC90lS_jtwcEghSyX2Ce=s48-c-k-c0xffffffff-no-rj-mo"
+          views="82.2K"
+          timestamp="2 days ago"
+        />
+
+        <VideoCard
+          title={`calling code bootcamps pretending I can't code...(They STRUGGLE pretty hard)"`}
+          image="https://i.ytimg.com/vi/SR1oCu9RhWI/hqdefault.jpg?sqp=-oaymwEZCOADEI4CSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCsauMDdu7PESeY_l4KbyVBFOybMA"
+          channel="Joshua Fluke"
+          channelImage="https://yt3.ggpht.com/a/AATXAJzsLfjQGNpQtVWS4hXGxM6F21cdArOL-sTIu9rOXw=s48-c-k-c0xffffffff-no-rj-mo"
+          views="84.2K"
+          timestamp="5 days ago"
+        />
+
+        <VideoCard
+          title={`🔴 Build a Whatsapp Clone with MERN Stack (MongoDB, Express, React, Node JS)`}
+          image="https://i.ytimg.com/vi/gzdQDxzW2Tw/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAu_J66n0_P1jy1eQlsgocO_zz9pQ"
+          channel="Clever Programmer"
+          channelImage="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s48-c-k-c0xffffffff-no-rj-mo"
+          views="140k"
+          timestamp="7 days ago"
+        />
+        <VideoCard
+          title={`TRASH or PASS! Joyner Lucas ( Snitch ) Evolution [REACTION!!!]`}
+          image="https://i.ytimg.com/vi/b8KxKTQSRU8/hq720.jpg?sqp=-oaymwEZCOgCEMoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLALUF8sOItosgOGNO-Yz21RIlnsAA"
+          channel="LayedBakDFR"
+          channelImage="https://yt3.ggpht.com/a/AATXAJxbd79agwiyCofooDIJh8HS0oN60wrEqNBXttsmQQ=s48-c-k-c0xffffffff-no-rj-mo"
+          views="115k"
+          timestamp="1 day ago"
+        />
+      </div>
     </div>
   );
 }
 
 export default RecommendedVideos;
+// title, views, channel, channelImage, timestamp, image;
