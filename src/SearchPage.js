952cc553eac65093927e7380576829ad5520b2a7
PGrover2001 <40870043+PGrover2001@users.noreply.github.com>
2020-10-24 15:22:11 +0100

    added the videoRow component and styled it

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./SearchPage.css";
 import TuneOutlinedIcon from "@material-ui/icons/TuneOutlined";
 import ChannelRow from "./ChannelRow";
+import VideoRow from "./VideoRow";
 function SearchPage() {
   return (
     <div className="searchPage">
@@ -19,6 +20,43 @@ function SearchPage() {
         description="You can find awesome programming lessons here! Also, expect programming tips and tricks that will take your coding skills to the next level."
       />
       <hr />
+
+      <VideoRow
+        views="2M"
+        subs="910k"
+        description="Do you want to become a six-figure JavaScript Developer? Check out the best JavaScript course on the planet 🌎 👇 "
+        title="How to Earn $1,000 as a Freelance Developer in 30 days (Part 2)"
+        timestamp="2hrs ago"
+        channel="Clever Programmer"
+        image="https://i.ytimg.com/vi/DFzP6gA0tkQ/hq720.jpg?sqp=-oaymwEZCOgCEMoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDf3CX-apGeRAaT6fLuy-HeM2FjqQ"
+      />
+      <VideoRow
+        views="2M"
+        subs="910k"
+        description="Do you want to become a six-figure JavaScript Developer? Check out the best JavaScript course on the planet 🌎 👇 "
+        title="How to Earn $1,000 as a Freelance Developer in 30 days (Part 2)"
+        timestamp="2hrs ago"
+        channel="Clever Programmer"
+        image="https://i.ytimg.com/vi/DFzP6gA0tkQ/hq720.jpg?sqp=-oaymwEZCOgCEMoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDf3CX-apGeRAaT6fLuy-HeM2FjqQ"
+      />
+      <VideoRow
+        views="2M"
+        subs="910k"
+        description="Do you want to become a six-figure JavaScript Developer? Check out the best JavaScript course on the planet 🌎 👇 "
+        title="How to Earn $1,000 as a Freelance Developer in 30 days (Part 2)"
+        timestamp="2hrs ago"
+        channel="Clever Programmer"
+        image="https://i.ytimg.com/vi/DFzP6gA0tkQ/hq720.jpg?sqp=-oaymwEZCOgCEMoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDf3CX-apGeRAaT6fLuy-HeM2FjqQ"
+      />
+      <VideoRow
+        views="2M"
+        subs="910k"
+        description="Do you want to become a six-figure JavaScript Developer? Check out the best JavaScript course on the planet 🌎 👇 "
+        title="How to Earn $1,000 as a Freelance Developer in 30 days (Part 2)"
+        timestamp="2hrs ago"
+        channel="Clever Programmer"
+        image="https://i.ytimg.com/vi/DFzP6gA0tkQ/hq720.jpg?sqp=-oaymwEZCOgCEMoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDf3CX-apGeRAaT6fLuy-HeM2FjqQ"
+      />
     </div>
   );
 }
